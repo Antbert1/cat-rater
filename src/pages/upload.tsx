ea5d6181813ca530f0884b178a7fdce77318b3f8
Antbert1 <anthea4002@gmail.com>
2021-09-30 20:48:47 +0100

    Adding back router

--- src/pages/upload.tsx
+++ src/pages/upload.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Loading from '../components/Loading';
 import UploadService from "../services/FileUploadService";
 import defaultImg from '../assets/blankCat.png';
-import back from '../assets/images/backChevron.png';
+import back from '../assets/backChevron.png';
 
-function Upload() {
+function Upload(props: any) {
     const [selectedFiles, setSelectedFiles] = useState("");
     const [imageSrc, setImageSrc] = useState(defaultImg);
     const [currentFile, setCurrentFile] = useState("");
@@ -30,23 +31,27 @@ function Upload() {
         setCurrentFile(currentFile);
 
         UploadService.upload(currentFile, (event: any) => {
+        }).then((response) => {
+            setMessage(response.data.message);
         })
-            .then((response) => {
-                setMessage(response.data.message);
-                // return UploadService.getFiles();
-            })
             .catch(() => {
-                // setProgress(0);
                 setMessage("Could not upload the file!");
                 setCurrentFile("");
             });
 
         setSelectedFiles("");
+        props.history.push('/');
     };
 
     return (
         <div>
             <Header />
+            <Link to="/">
+                <div className="back">
+                    <img src={back} alt={"back chevron"} />
+                    Back
+                </div>
+            </Link>
             <div>
                 <div className="catImage">
                     <img src={imageSrc} alt="blank cat" />
